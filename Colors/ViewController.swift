cef953a87bb093c2d465e18670ae6c467be02b28
David de Tena <daviddetenacalvo@gmail.com>
2017-02-20 01:31:59 +0100

    Updated UI for switcher on/off

--- Colors/ViewController.swift
+++ Colors/ViewController.swift
@@ -10,16 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - Properties
+    @IBOutlet weak var btnSwitch: UIButton!
+    @IBOutlet weak var imgKnobBase: UIImageView!
+    @IBOutlet weak var imgKnob: UIImageView!
+    
+    override var prefersStatusBarHidden: Bool{
+        return true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        imgKnob.isHidden = true
+        imgKnobBase.isHidden = true
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewWillAppear(_ animated: Bool) {
+        btnSwitch.setImage(#imageLiteral(resourceName: "img_switch_off"), for: .normal)
+        btnSwitch.setImage(UIImage(named:"img_switch_on"), for: .selected)
     }
-
-
+    
+    
+    @IBAction func btnSwitchPressed(_ sender: UIButton) {
+        btnSwitch.isSelected = !btnSwitch.isSelected
+        
+        if btnSwitch.isSelected {
+            // Show imgKnob
+            imgKnob.isHidden = false
+            imgKnobBase.isHidden = false
+            view.backgroundColor = UIColor(hue: 0.5, saturation: 0.5, brightness: 0.75, alpha: 1.0)
+        }
+        else{
+            // Dark gray background
+            view.backgroundColor = UIColor(hue: 0.5, saturation: 0, brightness: 0.2, alpha: 1.0)
+            imgKnob.isHidden = true
+            imgKnobBase.isHidden = true
+        }
+    }
 }
 
